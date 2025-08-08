41b402bcec3c4a427e7ee7d6f63b0f3fd1c14e9e
Jincheng.Sun <jincheng.sun@ca.ey.com>
2025-08-08 19:50:15 -0400

    refactor folder and finish archive and create script

--- azure_machine_learning/scripts/utils.py
+++ azure_machine_learning/scripts/utils.py
@@ -1,25 +1,34 @@
+import os
 import sys
 import logging
 import subprocess
 
 
-def get_logger(name: str, level: int = logging.INFO) -> logging.Logger:
+def get_logger(name: str, level: int = logging.INFO, log_dir="lazy_mlops_logs") -> logging.Logger:
     """Get a logger with the specified name and level. The logger will log to stdout.
 
     Args:
         name: str, The name of the logger.
-        level: int, The logging level.
+        level: int, default to logging.INFO level, The logging level.
+        log_dir: str, default to 'lazy_mlops_logs', logging file saving directory
 
     Returns:
         logging.Logger: The logger.
     """
     logger = logging.getLogger(name)
     logger.setLevel(level)
-    ch = logging.StreamHandler(sys.stdout)
-    ch.setLevel(level)
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    ch.setFormatter(formatter)
-    logger.addHandler(ch)
+    # Stream handler
+    stream_handler = logging.StreamHandler(sys.stdout)
+    stream_handler.setFormatter(formatter)
+    logger.addHandler(stream_handler)
+    # File handler
+    os.makedirs(log_dir, exist_ok=True)
+    sanitized_name = name.replace(".", "_")
+    log_path = os.path.join(log_dir, f"{sanitized_name}.log")
+    file_handler = logging.FileHandler(log_path)
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
     return logger
 
 
